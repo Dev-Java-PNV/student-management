5207d81098b5607ae8df4caf38bf9207cc38e899
HuynhDuc <duc.huynh25@student.passerellesnumeriques.org>
2024-07-03 12:05:31 +0700

    feature(read-enrollment-both-student-course): Doing EnrollmentView

---
+++ src/main/java/developer/views/EnrollmentView.java
@@ -0,0 +1,10 @@
+package developer.views;
+
+import developer.controller.EnrollmentController;
+
+public class EnrollmentView {
+    private final EnrollmentController enrollmentController;
+    public EnrollmentView() {
+        this.enrollmentController = new EnrollmentController();
+    }
+}
